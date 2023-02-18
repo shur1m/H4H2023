a0abe590499ed4ce225ecc7d7669eafd16984e0d
Anson Xu <85322327+shur1m@users.noreply.github.com>
2023-02-18 09:41:41 -0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.svg';
+import Flow from './Flow';
+
+
+function App() {
+  return (
+    <div style={{height:"100vh"}}>
+      <Flow/>
+    </div>
+  );
+}
+
+export default App;
