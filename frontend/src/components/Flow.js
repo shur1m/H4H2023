f9da7fc5a11eeef50443f43b66f87e542963fc60
Anson Xu <85322327+shur1m@users.noreply.github.com>
2023-02-18 15:13:10 -0800

    enable flow chart editing  buttons

--- frontend/src/components/Flow.js
+++ frontend/src/components/Flow.js
@@ -5,10 +5,14 @@ import ReactFlow, {
     applyNodeChanges,
     addEdge,
     useReactFlow,
+    useOnSelectionChange
 } from 'reactflow';
+
 import { useState, useCallback } from 'react';
+import {DocumentEditBar, DocumentEditButton} from './DocumentEdit';
 
 //WIP should be fetched from the backend
+let unusedId = 0;
 const initialNodes = [
     {
         id: '1',
@@ -28,7 +32,7 @@ const initialNodes = [
     },
   ];
   
-  const initialEdges = [
+const initialEdges = [
     {
         id: '1-2',
         source: '1',
@@ -36,40 +40,75 @@ const initialNodes = [
         label: 'to the',
         type: 'step',
     }
-  ];
+];
+
+// WIP add node to center of the viewport
+function handleAddNode(nodes, setNodes){
+    setNodes([...nodes, {
+        id: `node_${unusedId++}`,
+        position: { x: 0, y: 0 },
+        data: { label: 'new node' },
+        type: 'input'
+    }])
+}
 
 
-function Flow({initialEdges, initialNodes}) {
+// WIP somehow make sure reset viewport moves center to 0,0
+function handleResetViewPort(reactFlowInstance){
+    reactFlowInstance.setViewport({
+        x: 0,
+        y: 0,
+        zoom: 1,
+    })
+}
 
-    //setting up reactflow
+function DisplayWindowOnChange() {
+    useOnSelectionChange({
+        onChange: ({ nodes, edges }) => console.log('changed selection', nodes, edges),
+    });
+  
+    return null;
+}
+
+function Flow() {
+    // setting up reactflow
     const reactFlowInstance = useReactFlow();
-    console.log(reactFlowInstance.getEdges());
-
+    
+    // state for edges and nodes
     const [nodes, setNodes] = useState(initialNodes);
     const [edges, setEdges] = useState(initialEdges);
-
+    
+    // default functions for flow chart
     const onNodesChange = useCallback( (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),[] );
     const onEdgesChange = useCallback( (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),[] );
     const onConnect = useCallback((params) => {
         setEdges((eds) => addEdge(params, eds))
     },[]);
-
+    
+    DisplayWindowOnChange();
     return (
     <>
-        <div className='nodeEditBar'>something</div>
+        {/* setting up flow chart */}
         <div style={{ height: '100%' }}>
             <ReactFlow
-                nodes={initialNodes}
-                edges = {initialEdges}
+                nodes={nodes}
+                edges = {edges}
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
-                // i found it bois onInit
             >
             <Background />
             <Controls />
             </ReactFlow>
         </div>
+
+        {/* bar for editting nodes */}
+        <DocumentEditBar>
+            <DocumentEditButton label='add node' onClick={() => {handleAddNode(nodes, setNodes)}}/>
+            <DocumentEditButton label='save project'/>
+            <DocumentEditButton label='import image'/>
+            <DocumentEditButton label='reset viewport' onClick={() => handleResetViewPort(reactFlowInstance)}/>
+        </DocumentEditBar>
     </>
     );
 }
