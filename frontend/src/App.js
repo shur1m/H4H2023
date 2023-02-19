3698aa6051b7a9364c715a70e7a01adf6f852aa3
James Vong <j.vong2020@gmail.com>
2023-02-18 17:14:36 -0800

    Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import logo from './logo.svg';
 import FlowWithProvider from './components/FlowWithProvider';
 import 'reactflow/dist/style.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
-    <div style={{height:"100vh"}}>
-      <div style={{height: '100%'}}>
+    <div style={{height:"100vh"}} id="outer-container">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <div style={{height: '100%'}} id="page-wrap">
         <FlowWithProvider/>
       </div>
     </div>
